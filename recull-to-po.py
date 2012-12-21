e12e2240f4b99d8cfeda87850b4f9d9c5c044986
Jordi Mas <jmas@softcatala.org>
2012-12-21 21:51:21 +0100

    Fix formatting

--- recull-to-po.py
+++ recull-to-po.py
@@ -7,15 +7,15 @@ cvsfile = open("recull.csv", "r")
 pofile = polib.POFile()
 
 pofile.metadata = {
-    'Project-Id-Version': '1.0',
-    'Report-Msgid-Bugs-To': 'info@softcatala.org',
-    'POT-Creation-Date': '2007-10-18 14:00+0100',
-    'PO-Revision-Date': '2007-10-18 14:00+0100',
-    'Last-Translator': 'info@softcatala.org',
-    'Language-Team': 'Catalan <info@softcatala.org>',
-    'MIME-Version': '1.0',
-    'Content-Type': 'text/plain; charset=utf-8',
-    'Content-Transfer-Encoding': '8bit',
+	'Project-Id-Version': '1.0',
+	'Report-Msgid-Bugs-To': 'info@softcatala.org',
+	'POT-Creation-Date': '2007-10-18 14:00+0100',
+	'PO-Revision-Date': '2007-10-18 14:00+0100',
+	'Last-Translator': 'info@softcatala.org',
+	'Language-Team': 'Catalan <info@softcatala.org>',
+	'MIME-Version': '1.0',
+	'Content-Type': 'text/plain; charset=utf-8',
+	'Content-Transfer-Encoding': '8bit',
 	'Plural-Forms' : 'nplurals=2; plural=n != 1;',
 }
 
@@ -24,7 +24,6 @@ for line in cvsfile:
     columns = [col.strip() for col in columns]
 
     if columns:
-#		if ((columns[2] == 'm' or columns[2] == 'f') and '(' in columns[1] == False and '!' in columns[1] == False):
 		if ((columns[2] == 'm' or columns[2] == 'f') and not '(' in columns[1] and not '|' in columns[1]):
 
 			s = columns[0]
