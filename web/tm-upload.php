405a134bd76d5c896b2d82fe4d95a4ea206ac8ff
Jordi Mas <jmas@softcatala.org>
2013-01-20 16:54:56 +0100

    Ability to select diferent memories

--- web/tm-upload.php
+++ web/tm-upload.php
@@ -14,37 +14,50 @@ function showLink($link)
 	echo "<br/>Baixada: <a href=" . $link . ">" . $link . "</a>";	
 }
 
-// Main
+/***
+	Main
+***/
 
 	if ($_FILES["file"]["error"] > 0)
-	  {
-	  echo "Error: " . $_FILES["file"]["error"] . "<br>";
-	  }
-	else
-	  {
-	#  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-	#  echo "Type: " . $_FILES["file"]["type"] . "<br>";
-	#  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-	#  echo "Stored in: " . $_FILES["file"]["tmp_name"];
-	  }
+	{
+		echo "Error: " . $_FILES["file"]["error"] . "<br>";
+		die();
+	}
 	  
 	$po_outdir = "output";
 	  
 	if ( !file_exists($po_outdir) ) {
-	  mkdir ($po_outdir, 0777);
-	 }
+		mkdir ($po_outdir, 0777);
+	}
+	 
+	switch ($_POST["memory"])
+	{
+	case "gnome":
+		$tm_file = "gnome-tm.po";
+		break;
+	case "fedora":
+		$tm_file = "fedora-tm.po";
+		break;
+	case "mozilla":
+		$tm_file = "mozilla-tm.po";
+		break;
+	case "libreoffice":
+		$tm_file = "libreoffice-tm.po";
+		break;
+	default:
+		$tm_file = "tm.po";
+		break;
+	}
 	
 	$uploaded_file = $_FILES["file"]["tmp_name"];
 	$out_file = $_SERVER["DOCUMENT_ROOT"] . "/output/tm-" . $_FILES["file"]["name"];
-	$tm_file = $_SERVER["DOCUMENT_ROOT"] . "/tm.po";
+	$tm_file = $_SERVER["DOCUMENT_ROOT"] . "/memories/" . $tm_file;
 	$link = "http://recursos.softcatala.org" . "/output/tm-" . $_FILES["file"]["name"];
 	
-	$command = "msgmerge -N " . $uploaded_file . " " . $uploaded_file . " -C " . $tm_file . " > " . $out_file;
-	#echo $command;
+	$command = "msgmerge -N " . $uploaded_file . " " . $uploaded_file . " -C " . $tm_file . " > " . $out_file;	
 	exec($command, $output, $return_value);
 	
 	showStatistics($out_file);
 	showLink($link);
-
 
 ?> 
