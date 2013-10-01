d952852d1bfb10911b247c9d253c0dc60607b729
Gil Forcada <gforcada@gnome.org>
2013-10-02 00:22:49 +0200

    Make code more pythonic (10)
    - sort imports
    - do not add superfluous parenthesis around simple conditionals
    - keep lies within 80 characters
    - use the new string formatting
    - use single quotes whenever possible

--- src/project.py
+++ src/project.py
@@ -17,26 +17,26 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
-
-import os
-import logging
-
-from localfileset import LocalFileSet
-from compressedfileset import CompressedFileSet
 from bazaarfileset import BazaarFileSet
-from transifexfileset import TransifexFileSet
-from localdirfileset import LocalDirFileSet
-from filefileset import FileFileSet
-from subversionfileset import SubversionFileSet
-from polib import pofile
+from compressedfileset import CompressedFileSet
 from crawlerfileset import CrawlFileSet
+from filefileset import FileFileSet
+from localdirfileset import LocalDirFileSet
+from localfileset import LocalFileSet
+from polib import pofile
+from subversionfileset import SubversionFileSet
+from transifexfileset import TransifexFileSet
+
+import logging
+import os
+
 
 class Project:
 
     def __init__(self, name, filename):
         self.add_source = True
         self.filename = filename
-        self.filesets = list()
+        self.filesets = []
         self.name = name
 
     def get_filename(self):
@@ -46,41 +46,55 @@ class Project:
         self.add_source = add_source
 
     def _delete_po_file(self):
-        if (os.path.isfile(self.filename)):
-            os.system("rm " + self.filename)
+        if os.path.isfile(self.filename):
+            os.remove(self.filename)
 
     def add(self, fileset):
         fileset.set_tm_file(self.filename)
         self.filesets.append(fileset)
 
     def add_filesets(self, project_dto):
-
         for fileset in project_dto.filesets:
             logging.debug(fileset)
 
-            if (fileset.type == 'local-file'):
-                fs = LocalFileSet(fileset.name, fileset.url, fileset.target)
-            elif (fileset.type == 'compressed'):
-                fs = CompressedFileSet(fileset.name, fileset.url,
+            if fileset.type == 'local-file':
+                fs = LocalFileSet(fileset.name,
+                                  fileset.url,
+                                  fileset.target)
+            elif fileset.type == 'compressed':
+                fs = CompressedFileSet(fileset.name,
+                                       fileset.url,
                                        fileset.target)
                 fs.set_pattern(fileset.pattern)
-            elif (fileset.type == 'bazaar'):
-                fs = BazaarFileSet(fileset.name, fileset.url, fileset.target)
+            elif fileset.type == 'bazaar':
+                fs = BazaarFileSet(fileset.name,
+                                   fileset.url,
+                                   fileset.target)
                 fs.set_pattern(fileset.pattern)
-            elif (fileset.type == 'transifex'):
-                fs = TransifexFileSet(fileset.name, fileset.url,
+            elif fileset.type == 'transifex':
+                fs = TransifexFileSet(fileset.name,
+                                      fileset.url,
                                       fileset.target)
-            elif (fileset.type == 'local-dir'):
-                fs = LocalDirFileSet(fileset.name, fileset.url, fileset.target)
-            elif (fileset.type == 'file'):
-                fs = FileFileSet(fileset.name, fileset.url, fileset.target)
-            elif (fileset.type == 'subversion'):
-                fs = SubversionFileSet(fileset.name, fileset.url, fileset.target)
-            elif (fileset.type == 'crawl'):
-                fs = CrawlFileSet(fileset.name, fileset.url, fileset.target)
+            elif fileset.type == 'local-dir':
+                fs = LocalDirFileSet(fileset.name,
+                                     fileset.url,
+                                     fileset.target)
+            elif fileset.type == 'file':
+                fs = FileFileSet(fileset.name,
+                                 fileset.url,
+                                 fileset.target)
+            elif fileset.type == 'subversion':
+                fs = SubversionFileSet(fileset.name,
+                                       fileset.url,
+                                       fileset.target)
+            elif fileset.type == 'crawl':
+                fs = CrawlFileSet(fileset.name,
+                                  fileset.url,
+                                  fileset.target)
                 fs.set_pattern(fileset.pattern)
             else:
-                logging.error("Unsupported filetype: " + fileset.type)
+                msg = 'Unsupported filetype: {0}'
+                logging.error(msg.format(fileset.type))
 
             self.add(fs)
             fs.add_excluded(fileset.excluded)
@@ -94,37 +108,34 @@ class Project:
                 fileset.do()
 
         except Exception as detail:
-            logging.error("Project.do. Cannot complete " + self.filename)
+            msg = 'Project.do. Cannot complete {0}'
+            logging.error(msg.format(self.filename))
             logging.error(detail)
             self._delete_po_file()
 
     def statistics(self):
-
         words = 0
         entries = 0
 
         try:
-
             poFile = pofile(self.filename)
 
             for entry in poFile:
                 string_words = entry.msgstr.split(' ')
                 words += len(string_words)
 
-            entries = str(len(poFile.translated_entries()))
+            entries = len(poFile.translated_entries())
 
         except Exception as detail:
-            logging.error("Project. statistics exception " + self.filename)
+            msg = 'Project. statistics exception {0}'
+            logging.error(msg.format(self.filename))
             logging.error(detail)
 
-
         finally:
-            s = self.name + " project. " + str(entries)
-            s = s + " translated strings, words " + str(words)
-            logging.info(s)
+            msg = '{0} project. {1} translated strings, words {2}'
+            logging.info(msg.format(self.name, entries, words))
 
     def to_tmx(self):
-
         fileName, fileExtension = os.path.splitext(self.filename)
-        os.system("po2tmx " + self.filename + " --comment others -l ca-ES -o "
-                  + fileName + ".tmx")
+        cmd = 'po2tmx {0} --comment others -l ca-ES -o {1}.tmx'
+        os.system(cmd.format(self.filename, fileName))
