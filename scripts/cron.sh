2c239f54283f3d99219345fd759a420cd4143f1d
Jordi Mas <jmas@softcatala.org>
2013-11-02 14:18:36 +0100

    Update scripts

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -4,6 +4,16 @@ INTERMEDIATE_PO=/home/jmas/translation-memories/po
 INTERMEDIATE_TMX=/home/jmas/translation-memories/tmx
 PROGRAMS=/home/jmas/tm-git/src
 PROD_DIR=/var/www/recursos.softcatala.org/
+BACKUP_DIR=/home/jmas/previous
+
+# Catalan locale does not support thounsand separator
+export LC_ALL=ast_ES.utf-8
+
+# Copy existing PO files
+rm -r -f $BACKUP_DIR
+mkdir $BACKUP_DIR
+cd $BACKUP_DIR
+cp $INTERMEDIATE_PO/* $BACKUP_DIR
 
 # Download new translation files
 cd $PROGRAMS
@@ -12,6 +22,13 @@ rm -f *.tmx
 rm -f *.log
 python builder.py
 
+# Build aggregated memories
+cd $INTERMEDIATE_PO/
+python $PROGRAMS/builder.py -s $PROGRAMS/projects.json --all
+python $PROGRAMS/builder.py -s $PROGRAMS/projects.json --softcatala
+cp *.tmx $INTERMEDIATE_TMX/
+
+cd $PROGRAMS
 # Copy only new PO files
 for filename in *.po
   do
@@ -43,23 +60,16 @@ for filename in *.tmx
     fi
   done
 
-
-# Build aggregated memories
-cd $INTERMEDIATE_PO/
-python $PROGRAMS/builder.py -s $PROGRAMS/projects.json --all
-python $PROGRAMS/builder.py -s $PROGRAMS/projects.json --softcatala
-cp *.tmx ../tmx/
-
 # Update download file & index
 cd /home/jmas/tm-git/web/download-memories
-python download-creation.py -d ../../../translation-memories/po -t ../../../translation-memories/tmx
+python download-creation.py -d $INTERMEDIATE_PO -t $INTERMEDIATE_TMX
 cd ../search
-python index-creation.py -d ../../../translation-memories/po
+python index-creation.py -d $INTERMEDIATE_PO
 
 # Notify completion
 cd $PROGRAMS
 pwd
-python compare-sets.py -s $PROD_DIR -t $INTERMEDIATE_PO > report.txt
+python compare-sets.py -s  $BACKUP_DIR -t $INTERMEDIATE_PO > report.txt
 ls report.txt -l
 cat report.txt | mail -s "Recursos updated" "jmas@softcatala.org" 
 
