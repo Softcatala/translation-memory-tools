c576b2cb45809c2d39edf58d1f6a0a51abe7c185
Jordi Mas <jmas@softcatala.org>
2013-11-03 16:45:17 +0100

    Update scripts

---
+++ scripts/build.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+ROOT=/home/jmas
+
+if [ ! -z "$DEVENV" ]; then
+    ROOT=/home/jmas/dev
+    echo Development enviroment set to $ROOT
+fi
+
+INTERMEDIATE_PO=$ROOT/translation-memories/po
+INTERMEDIATE_TMX=$ROOT/translation-memories/tmx
+PROGRAMS=$ROOT/tm-git/src
+BACKUP_DIR=$ROOT/previous
+
+# Catalan locale does not support thounsand separator
+export LC_ALL=ast_ES.utf-8
+
+# Copy existing PO files
+rm -r -f $BACKUP_DIR
+mkdir $BACKUP_DIR
+cd $BACKUP_DIR
+cp $INTERMEDIATE_PO/* $BACKUP_DIR
+
+# Download new translation files
+cd $PROGRAMS
+rm -f *.po
+rm -f *.tmx
+rm -f *.log
+python builder.py
+
+# Build aggregated memories
+cd $INTERMEDIATE_PO/
+python $PROGRAMS/builder.py -s $PROGRAMS/projects.json --all
+python $PROGRAMS/builder.py -s $PROGRAMS/projects.json --softcatala
+cp *.tmx $INTERMEDIATE_TMX/
+
+cd $PROGRAMS
+# Copy only new PO files
+for filename in *.po
+  do
+    # If file exists and size is greater than 200 bytes
+    if [ -e  $filename ]
+      then
+        fsize=$(du -b "$filename" | cut -f 1)
+        if [ $fsize -ge 200 ];
+          then
+	    if ! diff -q $filename $INTERMEDIATE_PO/$filename > /dev/null
+	      then
+	        echo Copying $filename
+                cp $filename $INTERMEDIATE_PO/$filename
+            fi
+        fi 
+    fi
+  done
+
+
+# Copy only new TMX files
+for filename in *.tmx
+  do
+    # If file exists and size is greater than 200 bytes
+    if [ -e  $filename ]
+      then
+        fsize=$(du -b "$filename" | cut -f 1)
+        if [ $fsize -ge 200 ];
+          then
+            echo Copying $filename
+            cp $filename $INTERMEDIATE_TMX/$filename
+        fi
+    fi
+  done
+
+# Update download file & index
+cd $ROOT/tm-git/web/download-memories
+python download-creation.py -d $INTERMEDIATE_PO -t $INTERMEDIATE_TMX
+cd ../search
+python index-creation.py -d $INTERMEDIATE_PO
+
+# Notify completion
+cd $PROGRAMS
+pwd
+python compare-sets.py -s  $BACKUP_DIR -t $INTERMEDIATE_PO > report.txt
+ls report.txt -l
+cat report.txt | mail -s "Recursos updated" "jmas@softcatala.org" 
+
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-TARGET_DIR=/var/www/recursos.softcatala.org/
+TARGET_DIR=/var/www/recursos.softcatala.org
+ROOT=/home/jmas
+
+if [ ! -z "$DEVENV" ]; then
+    ROOT=/home/jmas/dev
+    TARGET_DIR=/var/www/recursos.softcatala.org/dev
+    echo Development enviroment set to $TARGET_DIR
+fi
 
 # Index
 rm -r -f $TARGET_DIR/indexdir
 mkdir $TARGET_DIR/indexdir
-cp -r tm-git/web/search/indexdir/* $TARGET_DIR/indexdir
+cp -r $ROOT/tm-git/web/search/indexdir/* $TARGET_DIR/indexdir
 
 # Search TM app
-cp tm-git/web/apply_tm/recursos.css $TARGET_DIR
-cp tm-git/web/search/index.html $TARGET_DIR
-cp tm-git/web/search/web_search.py $TARGET_DIR
-cp tm-git/web/search/statistics.html $TARGET_DIR
-cp tm-git/web/download-memories/download.html $TARGET_DIR
+cp $ROOT/tm-git/web/apply_tm/recursos.css $TARGET_DIR
+cp $ROOT/tm-git/web/search/index.html $TARGET_DIR
+cp $ROOT/tm-git/web/search/web_search.py $TARGET_DIR
+cp $ROOT/tm-git/web/search/statistics.html $TARGET_DIR
+cp $ROOT/tm-git/web/download-memories/download.html $TARGET_DIR
 
 # Download memories
-cp tm-git/web/download-memories/download.html $TARGET_DIR
+cp $ROOT/tm-git/web/download-memories/download.html $TARGET_DIR
 rm -r -f $TARGET_DIR/memories
 mkdir $TARGET_DIR/memories
-cp tm-git/web/download-memories/memories/*.zip $TARGET_DIR/memories
-cp tm-git/src/report.txt $TARGET_DIR
+cp $ROOT/tm-git/web/download-memories/memories/*.zip $TARGET_DIR/memories
+cp $ROOT/tm-git/src/report.txt $TARGET_DIR
