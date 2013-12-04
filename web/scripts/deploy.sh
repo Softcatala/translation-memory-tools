cadc974a3cd29961aef7fa6045c2650bc3ba3b98
Jordi Mas <jmas@softcatala.org>
2013-12-04 09:50:10 +0100

    Support for pre-prod enviroment

--- web/scripts/deploy.sh
+++ web/scripts/deploy.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
-TARGET_DIR=/var/www/recursos.softcatala.org
+
+#
+# Build a pre-production enviroment where we can run integration tests
+#
+
+TARGET_DIR=/var/www/recursos.softcatala.org/preprod
 ROOT=/home/jmas
 
 if [ ! -z "$DEVENV" ]; then
     ROOT=/home/jmas/dev
-    TARGET_DIR=/var/www/recursos.softcatala.org/dev
     echo Development enviroment set to $TARGET_DIR
     INTEGRATION_PARAM=-d
 fi
 
+# Index
+rm -r -f $TARGET_DIR/indexdir
+mkdir $TARGET_DIR/indexdir
+cp -r $ROOT/tm-git/web/search/indexdir/* $TARGET_DIR/indexdir
+
+# Search TM app
+cp $ROOT/tm-git/web/search/recursos.css $TARGET_DIR
+cp $ROOT/tm-git/web/search/index.html $TARGET_DIR
+cp $ROOT/tm-git/web/search/web_search.py $TARGET_DIR
+cp $ROOT/tm-git/web/search/statistics.html $TARGET_DIR
+cp $ROOT/tm-git/web/download-memories/download.html $TARGET_DIR
+
+# Download memories
+cp $ROOT/tm-git/web/download-memories/download.html $TARGET_DIR
+rm -r -f $TARGET_DIR/memories
+mkdir $TARGET_DIR/memories
+cp $ROOT/tm-git/web/download-memories/memories/*.zip $TARGET_DIR/memories
+cp $ROOT/tm-git/src/report.txt $TARGET_DIR
+
+# Run integration tests
 cd $ROOT/tm-git/integration-tests/
 python run.py $INTEGRATION_PARAM
 
@@ -16,6 +40,19 @@ RETVAL=$?
 if [ $RETVAL -ne 0 ]; then 
    echo Aborting deployment
    exit
+fi
+
+#
+# Deployment of final version
+# 
+
+TARGET_DIR=/var/www/recursos.softcatala.org
+ROOT=/home/jmas
+
+if [ ! -z "$DEVENV" ]; then
+    ROOT=/home/jmas/dev
+    TARGET_DIR=/var/www/recursos.softcatala.org/dev
+    echo Development enviroment set to $TARGET_DIR
 fi
 
 # Index
