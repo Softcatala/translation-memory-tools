02f7ee6e092a27ff541178bc1c6a76d447f79670
Jordi Mas <jmas@softcatala.org>
2014-04-02 20:53:53 +0200

    Deploy terminology

--- deployment/cron.sh
+++ deployment/cron.sh
@@ -2,3 +2,4 @@ export DEVENV=1
 PATH=$PATH:/usr/local/bin/
 sh build.sh
 sh deploy.sh
+sh deploy-terminology.sh
---
+++ deployment/deploy-terminology.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -z "$DEVENV" ]; then
+    echo Not ready for production yet 
+    exit
+fi
+
+echo Deploy terminology
+ROOT=/home/jmas/dev
+TARGET_DIR=/var/www/recursos.softcatala.org/dev/terminologia
+
+cd $ROOT/tm-git/terminology
+
+# Get files
+cd sc-tm
+rm *.po
+cp $ROOT/tm-git/web/search/memories/softcatala-tm.po.zip .
+unzip softcatala-tm.po.zip
+rm -f softcatala-tm.po
+cd ..
+
+cd all-tm
+rm *.po
+cp $ROOT/tm-git/web/search/memories/tots-tm.po.zip .
+unzip tots-tm.po.zip
+rm -f tots-tm.po
+cd ..
+
+# Build
+python term-extract.py -s sc-tm -t sc-glossary -c "Glossari construït a partir de les memòries dels projectes de Softcatalà" 
+
+python term-extract.py -s all-tm -t all-glossary -c "Glossari construït a partir de les memòries de tots els projectes"
+
+# Deploy
+cp *.html $TARGET_DIR
+cp *.csv $TARGET_DIR
