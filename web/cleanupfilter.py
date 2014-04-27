18f1cee27a63bf28a32cd5ab240f469d668af9b7
Jordi Mas <jmas@softcatala.org>
2014-04-27 09:16:28 +0200

    Cleans CleanUpFilter

--- web/cleanupfilter.py
+++ web/cleanupfilter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 #
-# Copyright (c) 2013 Jordi Mas i Hernandez <jmas@softcatala.org>
+# Copyright (c) 2014 Jordi Mas i Hernandez <jmas@softcatala.org>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -18,38 +18,17 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 02111-1307, USA.
 
-import sys
-import time
-import os
 from whoosh.fields import *
-from whoosh.index import create_in
 from whoosh.analysis import *
 from cleanstring import CleanString
 
 class CleanUpFilter(Filter):
-    """Uses unicode.lower() to lowercase token text.
-
-    >>> rext = RegexTokenizer()
-    >>> stream = rext("This is a TEST")
-    >>> [token.text for token in LowercaseFilter(stream)]
-    ["this", "is", "a", "test"]
-    """
-
-
-    def _clean_string(self, result):
-
-        chars = {'_', '&', '~',  # Accelerators
-                 ':', ',', '...', u'…'  # Punctuations
-        }
-
-        for c in chars:
-            result = result.replace(c, '')
-
-        return result
+    '''
+        Clean ups acceleators when generating the tokens for the index
+        allowing to ignore them
+    '''
 
     def __call__(self, tokens):
         for t in tokens:
             t.text = CleanString.get(t.text)
-            yield t    
-
-
+            yield t
