a4c7cd29299f85cf0b1c5f640e09af170422fa5f
Leandro Regueiro <leandro.regueiro@gmail.com>
2014-11-23 13:26:33 +0100

    Cleanup: Minor comments adjustments

--- src/web/download-creation.py
+++ src/web/download-creation.py
@@ -210,12 +210,12 @@ def build_invidual_projects_memory(json, memories):
 
 
 def process_template(template, filename, ctx):
-    # Load template and process it
+    # Load template and process it.
     template = open(template, 'r').read()
     parsed = pystache.Renderer()
     s = parsed.render(unicode(template, "utf-8"), ctx)
 
-    # Write output
+    # Write output.
     f = open(filename, 'w')
     f.write(s.encode("utf-8"))
     f.close()
