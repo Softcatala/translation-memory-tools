8c1126814059d5191df6f4c9f128e70485ea1049
Jordi Mas i Hernàndez <jmas@recursos.softcatala.org>
2015-05-01 12:04:41 +0200

    Update deploy script

--- deployment/generate-and-deploy.sh
+++ deployment/generate-and-deploy.sh
@@ -5,9 +5,12 @@
 PATH=$PATH:/usr/local/bin/
 
 ROOT=/home/jmas/dev
-DEPLOY_DIR=/home/jmas/web/recursos
+DEPLOY_DIR=/home/jmas/web/recursos-dev
 PREPROD_DEPLOY_DIR=/home/jmas/web/recursos-preprod
 
+#export NOPOBUILD=1
+
+export LANG="ca_ES.UTF-8"
 source /home/jmas/web/python-env/bin/activate
 
 # Auto pull changes for dev
@@ -15,13 +18,13 @@ cd $ROOT/tm-git
 git pull
 cd $ROOT
 
-sh $ROOT/tm-git/deployment/generate-tm.sh $ROOT
-sh $ROOT/tm-git/deployment/generate-terminology.sh $ROOT
-sh $ROOT/tm-git/deployment/generate-lt.sh $ROOT
+bash $ROOT/tm-git/deployment/generate-tm.sh $ROOT
+bash $ROOT/tm-git/deployment/generate-terminology.sh $ROOT
+bash $ROOT/tm-git/deployment/generate-lt.sh $ROOT
 
 # Since pology is installed in the system Python
 deactivate
-sh $ROOT/tm-git/deployment/generate-pology.sh $ROOT
+bash $ROOT/tm-git/deployment/generate-pology.sh $ROOT
 source /home/jmas/web/python-env/bin/activate
 
-sh $ROOT/tm-git/deployment/deploy.sh $ROOT $DEPLOY_DIR $PREPROD_DEPLOY_DIR
+bash $ROOT/tm-git/deployment/deploy.sh $ROOT $DEPLOY_DIR $PREPROD_DEPLOY_DIR
