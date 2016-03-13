4117b215c92c53512f921c73d1862c2afc575267
Jordi Mas <jmas@softcatala.org>
2016-03-13 18:05:19 +0100

    Add out directory to newly added fileset

--- src/builder/transifexhubfileset.py
+++ src/builder/transifexhubfileset.py
@@ -165,6 +165,7 @@ class TransifexHubFileSet(FileSet):
                 url = transifex_url
                 url = url + self._clean_string(option)
                 fileset = TransifexFileSet(self.project_name, option, url, '')
+                fileset.set_out_directory(self.out_directory)
                 self.project.add(fileset)
 
             # All the new filesets have been added re-process project now
