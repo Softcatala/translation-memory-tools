1259c31392c90c84704aaa711f982a82aa2d0149
Jordi Mas <jmas@softcatala.org>
2018-01-14 21:53:15 +0100

    Add unittest for TMX conversion

---
+++ src/tests/builder-tests/testconverttmx.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2018 Jordi Mas i Hernandez <jmas@softcatala.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+# Boston, MA 02111-1307, USA.
+
+from builder.converttmx import ConvertTmx
+import unittest
+import polib
+import tempfile
+from os import path
+
+
+class TestConvertTmx(unittest.TestCase):
+
+    def _get_files(self):
+        tmx_file = path.dirname(path.realpath(__file__))
+        tmx_file += '/data/conversions/tmx/test.tmx'
+
+        tmpfile = tempfile.NamedTemporaryFile()
+        po_filename = tmpfile.name + ".po"
+
+        return tmx_file, po_filename
+
+    def test_convertion(self):
+        tmx_file, po_filename = self._get_files()
+        convertTmx = ConvertTmx(tmx_file, po_filename)
+        convertTmx.convert()
+
+        entries = polib.pofile(po_filename)
+
+        self.assertEquals(len(entries), 2)
+        self.assertEquals(entries[0].msgid, "Modern visual refresh")
+        self.assertEquals(entries[0].msgstr, "Una renovació visual moderna")
+
+        self.assertEquals(entries[1].msgid, "As You Like It")
+        self.assertEquals(entries[1].msgstr, "Com vulgueu")
+
+
+if __name__ == '__main__':
+    unittest.main()
