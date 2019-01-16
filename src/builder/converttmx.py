a7bba221ed3fa80865ef412bcb44b7144ea9e173
Jordi Mas <jmas@softcatala.org>
2019-01-16 17:47:58 +0100

    Tmx conversion info in file not specific to Mozilla

--- src/builder/converttmx.py
+++ src/builder/converttmx.py
@@ -32,11 +32,11 @@ class ConvertTmx():
 
         pofile.metadata = {
             'Project-Id-Version': '1.0',
-            'Report-Msgid-Bugs-To': 'info@mozilla.org',
+            'Report-Msgid-Bugs-To': 'none',
             'POT-Creation-Date': '2007-10-18 14:00+0100',
             'PO-Revision-Date': '2007-10-18 14:00+0100',
-            'Last-Translator': 'info@termcat.cat',
-            'Language-Team': 'Catalan <info@mozilla.org>',
+            'Last-Translator': 'none@none.org',
+            'Language-Team': 'Catalan <info@none.org>',
             'MIME-Version': '1.0',
             'Content-Type': 'text/plain; charset=utf-8',
             'Content-Transfer-Encoding': '8bit',
