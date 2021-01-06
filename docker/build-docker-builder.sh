39aeaa94fd18b4c56b20822ad9789491412743e9
Jordi Mas <jmas@softcatala.org>
2021-01-06 16:52:13 +0100

    Slim

--- docker/build-docker-builder.sh
+++ docker/build-docker-builder.sh
@@ -1,4 +1,4 @@
 pushd ../
-docker build -t tmt-builder . -f docker/dockerfile-builder
+docker build -t tmt-builder . -f docker/dockerfile-builder --no-cache
 popd
 docker image ls | grep tmt-builder
