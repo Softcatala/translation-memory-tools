cea32905fa66761f4c31dabc837f4d6af17fd29b
Jordi Mas <jmas@softcatala.org>
2022-01-28 20:51:47 +0100

    Check that license field is SPDX complaint

--- src/tests/cfg-validation-tests/testcfgvalidation.py
+++ src/tests/cfg-validation-tests/testcfgvalidation.py
@@ -20,8 +20,23 @@ from builder.jsonbackend import JsonBackend
 from builder.projects import Projects
 import unittest
 from os import path
+import json
+
 
 class TestCfgValidation(unittest.TestCase):
+
+    def _load_valid_licenses_ids(self):
+        projects_dir = path.dirname(path.realpath(__file__))
+        filename = path.join(projects_dir, "licenses.json")
+        licenses_ids = set()
+        with open(filename) as json_file:
+            data = json.load(json_file)
+            licenses = data['licenses']
+            for license in licenses:
+                licenses_ids.add(license['licenseId'])
+
+        licenses_ids.add("propietària")
+        return licenses_ids
 
     def test_cfg_validation(self):
         projects_dir = path.dirname(path.realpath(__file__))
@@ -36,6 +51,20 @@ class TestCfgValidation(unittest.TestCase):
         for project_dto in json.projects:
             projects.add_project(project_dto, add_source = True)
 
+    def test_check_license(self):
+        projects_dir = path.dirname(path.realpath(__file__))
+        projects_dir += '/../../../cfg/projects/'
+        json = JsonBackend(projects_dir)
+        json.load()
+
+        valid_licenses_ids = self._load_valid_licenses_ids()
+        for project in json.projects:
+            licenses_ids = [x.strip() for x in project.license.split(',')]
+            for license_id in licenses_ids:
+                if len(license_id) == 0:
+                    continue
+
+                self.assertIn(license_id, valid_licenses_ids)
 
 if __name__ == '__main__':
     unittest.main()
