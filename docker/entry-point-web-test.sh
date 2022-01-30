97ac18c6bd10784c4c87490264dbc4aa6c910647
Jordi Mas <jmas@softcatala.org>
2022-01-30 21:52:29 +0100

    Use httping instead of sleep to wait for a process

--- docker/entry-point-web-test.sh
+++ docker/entry-point-web-test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /srv/web
 gunicorn web_search:app -b 0.0.0.0:8080 &
-sleep 20s
+while ! httping -qc1 http://localhost:8080 ; do sleep 1 ; echo "Waiting for server to respond"; done
 cd /srv/integration-tests
 python run.py -e localhost
 
