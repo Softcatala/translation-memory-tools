0b2acdb6930a0abcd7f655a67dcdd2a1fec59e8a
Jordi Mas <jmas@softcatala.org>
2023-09-02 08:42:50 +0200

    Add unit tests for php conversion

---
+++ src/tests/builder-tests/data/conversions/php/ca/user.php
@@ -0,0 +1,9 @@
+<?php
+$string['addcondition'] = 'Afegeix una condició';
+$string['adverbfor_and'] = 'i';
+$string['adverbfor_andnot'] = 'i';
+$string['adverbfor_or'] = 'o';
+$string['applyfilters'] = 'Aplica filtres';
+$string['clearfilterrow'] = 'Suprimeix la fila de filtres';
+$string['clearfilters'] = 'Neteja els filtres';
+
