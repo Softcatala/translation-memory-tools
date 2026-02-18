7aa4d4b5579a58de2ab04f6c8cd1747d2ae14566
Pere Orga <pere@orga.cat>
2026-02-18 18:38:20 +0100

    feat: new projects

--- src/builder/convertfiles.py
+++ src/builder/convertfiles.py
@@ -163,7 +163,7 @@ class ConvertFiles:
                     break
 
             src = None
-            for filename in ["en.ini", "EN.ini", "en_GB.ini", "en-US.ini"]:
+            for filename in ["en.ini", "EN.ini", "en_GB.ini", "en_US.ini", "en-US.ini"]:
                 fullName = "{0}/{1}".format(dirName, filename)
                 if os.path.isfile(fullName):
                     src = fullName
@@ -278,10 +278,12 @@ class ConvertFiles:
             ("ca.json", "en.json", "ca.json"),
             ("ca_ES.json", "en.json", "ca_ES.json"),
             ("ca.json", "en-US.json", "ca.json"),
+            ("ca-ES.json", "en-US.json", "ca-ES.json"),
             ("ca.i18n.json", "en.i18n.json", "ca.i18n.json"),
             ("main-ca.json", "main.json", "main-ca.json"),
             ("strings_ca.i18n.json", "strings.i18n.json", "strings_ca.i18n.json"),
             ("strings_ca.json", "strings.json", "strings_ca.json"),
+            ("lang-ca.json", "lang-en.json", "lang-ca.json"),
         ]
 
         for spec, src, tgt in files:
